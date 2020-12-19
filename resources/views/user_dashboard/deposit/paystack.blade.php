722dd8178c6d615e409e3539ce1ba8c72c09638e
blazefluz <akpomudje@gmail.com>
2020-12-19 14:10:43 +0000

    initial commit

--- resources/views/user_dashboard/deposit/paystack.blade.php
+++ resources/views/user_dashboard/deposit/paystack.blade.php
@@ -9,15 +9,15 @@
                         <div class="card-header"><h4 class="float-left">@lang('message.dashboard.deposit.deposit-stripe-form.title')</h4></div>
                         <div class="card-body">
                             <form method="POST" action="{{ route('pay') }}" accept-charset="UTF-8" class="form-horizontal" role="form">
-                                {{ csrf_field() }} 
+                                @csrf
                                 <div class="row" style="margin-bottom:40px;">
                                     <div class="col-md-8 col-md-offset-2">
-                                     {{dd($data)}}
-                                        <input type="hidden" name="email" value="{{$email}}"> {{-- required --}}
+                                     
+                                        <input type="hidden" name="email" value="{{$data->email}}"> {{-- required --}}
                                         <input type="hidden" name="orderID" value="345">
-                                        <input type="hidden" name="amount" value="{{$amount}}"> {{-- required in kobo --}}
-                                        <input type="hidden" name="currency" value="{{$currencyCode}}">
-                                        <input type="hidden" name="reference" value="{{ $reference }}"> {{-- required --}}
+                                        <input type="hidden" name="amount" value="{{$data->amount}}"> {{-- required in kobo --}}
+                                        <input type="hidden" name="currency" value="{{$data->currencyCode}}">
+                                        <input type="hidden" name="reference" value="{{ $data->reference }}"> {{-- required --}}
                                      {{-- works only when using laravel 5.1, 5.2 --}}
                             
                                        
